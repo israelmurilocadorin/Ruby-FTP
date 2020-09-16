156000dba23a03ba00be38f20c064d6344d8eadf
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2020-09-15 23:31:21 -0300

    delete

--- teste.rb
+++ teste.rb
@@ -1,28 +1,15 @@
-require "down"
-require "fileutils"
-require 'net/sftp'
-
 class Client
-
-    @@lista = Array.new
-    @@img = ""
-
-    def self.move()
-    end
     
-    def self.down()
-        @@img = Down.download("https://plantasdecasas.com/storage/2017/07/planta-fachada-casa-115-fr-AG.jpg") 
-        aa = @@img            #,destination: "C:/Users/israe/Desktop/FTP/arquivos")
-        FileUtils.mv(img.path, "C:/Users/israe/Desktop/FTP/arquivos#{img.original_filename}")
-        puts @@img
+    def self.delete(diretorio)
+        
+        if(File.exist?(diretorio))
+            File.delete(diretorio)
+            puts "> Deletado com sucesso <"
+        else
+            puts "> Arquivo inexistente <"
+        end
     end
 
 end
 
-Client.down()
-#Client.down()
-
-
-
-#C:/Users/israe/Desktop/FTP/arquivos/ggg.txt
-#C:/Users/israe/Downloads
+Client.delete("C:/Users/israe/Desktop/FTP/arquivos/ggg.txt")
